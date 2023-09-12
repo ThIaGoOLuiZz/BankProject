d13ea07eb0477c1a609cd5a13e25a2cca25620f0
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2023-09-11 22:57:44 -0300

    Initial commit

---
+++ src/Movement.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Movement {
+    private static String move;
+
+    public String getMoves() {
+        return move;
+    }
+
+    public void setMoves(String moves) {
+        this.move = moves;
+    }
+}
